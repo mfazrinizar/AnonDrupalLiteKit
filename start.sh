3de8a63f87b4e941d45d2672ecb351c2d420fbaf
Anon6372098 <anon6372098.id@gmail.com>
2018-12-07 22:30:48 +0700

    Main Start

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ clear
 biru='\033[34;1m'
 hijau='\033[32;1m'                                        
 ungu='\033[35;1m'
-abu-abu='\033[36;1m'
 merah='\033[31;1m'
 putih='\033[37;1m'                                           
 kuning='\033[33;1m'
@@ -16,7 +15,7 @@ date
 echo ""      
 echo "\033[32;1mCreator : Anon6372098" 
 echo "\033[35;1mTeam : D4RK SYST3M F41LUR3 S33K3R"
-echo "\033[36;1mThanks to : All Member of DSFS"
+echo "\033[32;1mThanks to : All Member of DSFS"
 echo "\033[37;1mAlly Team : DSFS Public"
 echo "\033[33;1mContact Me : anon6372098.id@gmail.com"
 echo "\033[31;1mSubscribe Youtube :"
@@ -38,16 +37,16 @@ then
 clear
 figlet "Anon Admin Finder" | lolcat
 echo "\033[32;1m"
-python2 Anon.py
+python2 anon.py
 fi
 
 if [ $anon = Q ] || [ $anon = q ]
 then
 echo "\033[32;1mWe Are D4RK SYST3M F41LUR3 S33K3R (DSFS)"
 sleep 1
-echo "\033[32;1mWe Are Bug Seeker"
+echo "\033[31;1mWe Are Bug Seeker"
 sleep 1
-echo "\033[32;1mWe Are Bug Creator"
+echo "\033[31;1mWe Are Bug Creator"
 sleep 1
 echo "\033[32;1mPerbanyaklah Membuat, Kurangilah Merusak"
 sleep 1
