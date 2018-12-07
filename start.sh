930296e374aa3d5cd4787e2444ed435cc50add40
Anon6372098 <anon6372098.id@gmail.com>
2018-12-07 22:24:57 +0700

    Main Start

---
+++ start.sh
@@ -0,0 +1,55 @@
+#!/system/xbin/bash
+clear
+biru='\033[34;1m'
+hijau='\033[32;1m'                                        
+ungu='\033[35;1m'
+abu-abu='\033[36;1m'
+merah='\033[31;1m'
+putih='\033[37;1m'                                           
+kuning='\033[33;1m'
+sleep 1
+toilet -f slant -F gay "Anon Drupal" | lolcat
+toilet -f slant -F gay "LiteKit" | lolcat
+echo "\033[34;1m"
+cal
+date
+echo ""      
+echo "\033[32;1mCreator : Anon6372098" 
+echo "\033[35;1mTeam : D4RK SYST3M F41LUR3 S33K3R"
+echo "\033[36;1mThanks to : All Member of DSFS"
+echo "\033[37;1mAlly Team : DSFS Public"
+echo "\033[33;1mContact Me : anon6372098.id@gmail.com"
+echo "\033[31;1mSubscribe Youtube :"
+echo "\033[33;1mD4RK SYST3M F41LUR3 S33K3R" "\033[36;1m(DSFS)"
+echo "\033[34;1m[============================]"
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Tool :"
+echo "\033[32;1m"
+echo "[===============================================]"
+echo "A.> Anon Drupal LiteKit"
+echo "[===============================================]"
+echo "Q.> Exit/Keluar"
+echo "[===============================================]"
+read -p "Anon Admin Finder]-->" anon
+
+if [ $anon = A ] || [ $anon = a ]
+then
+clear
+figlet "Anon Admin Finder" | lolcat
+echo "\033[32;1m"
+python2 Anon.py
+fi
+
+if [ $anon = Q ] || [ $anon = q ]
+then
+echo "\033[32;1mWe Are D4RK SYST3M F41LUR3 S33K3R (DSFS)"
+sleep 1
+echo "\033[32;1mWe Are Bug Seeker"
+sleep 1
+echo "\033[32;1mWe Are Bug Creator"
+sleep 1
+echo "\033[32;1mPerbanyaklah Membuat, Kurangilah Merusak"
+sleep 1
+exit
+fi
